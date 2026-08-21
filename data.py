1942319f4484e382f3c14ac6fd88dcaab07f8f6b
Kunal <kunal13shelke@gmail.com>
2026-08-21 21:04:30 +0530

    added preprocessed data

---
+++ data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_data():
+
+    iris = load_iris()
+    x = iris.data
+    y = iris.target
+
+    return x, y
+
+def data_preprocessing():
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=42)
+
+    scaler = StandardScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return X_train_scaled, X_test_scaled, y_train, y_test
